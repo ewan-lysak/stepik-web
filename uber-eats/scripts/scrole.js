7a1ddb83de472e457cc4976f2b06791b52be8f0b
ewan-lysak <ewan.lysak@yandex.ru>
2022-05-31 17:43:26 +0300

    d

--- uber-eats/scripts/scrole.js
+++ uber-eats/scripts/scrole.js
@@ -1,5 +1,15 @@
 function handleButtonClick() {
-    document.getElementById("header-1").scrollIntoView({block: "end", behavior: "smooth"});
+    document.getElementById("unc").scrollIntoView({block: "end", behavior: "smooth"});
 }
 
 document.querySelector('.btn-up').addEventListener('click', handleButtonClick);
+
+function hideBlock() {
+    if (window.scrollY() >= 1200) {
+        document.getElementById("btn-up").style.display = 'none';
+    }
+    else {
+        document.getElementById("btn-up").style.display = '';
+    }
+  }
+  window.scroll(hideBlock);
