ec08775802f89f26aac8c2962d1a69ed584b1c49
ewan-lysak <ewan.lysak@yandex.ru>
2022-06-03 12:29:03 +0300

    i

--- uber-eats/scripts/hide.js
+++
@@ -1,1 +0,0 @@
-document.getElementById(btn-up).style.visibility = "hidden";
--- uber-eats/scripts/scrole.js
+++ uber-eats/scripts/scrole.js
@@ -1,20 +1,19 @@
+const btnUp = document.querySelector(".btn-up");
 function handleButtonClick() {
-    document.getElementById("unc").scrollIntoView({block: "end", behavior: "smooth"});
+  document
+    .querySelector("body")
+    .scrollIntoView({ block: "start", behavior: "smooth" });
 }
 
-document.querySelector('.btn-up').addEventListener('click', handleButtonClick);
+document.querySelector(".btn-up").addEventListener("click", handleButtonClick);
 
 function hide() {
-    if (window.scrollY >= window.outerHeight / 1.6) {
-        document.getElementById("btn-up").classList.add('hide');
-    }
-    else {
-        document.getElementById("btn-up").classList.remove('hide');
-    }
+  if (window.scrollY >= window.outerHeight / 1.6) {
+    document.querySelector(".btn-up").classList.add("hide");
+  } else {
+    document.querySelector(".btn-up").classList.remove("hide");
   }
- hide();
-  window.addEventListener("scroll", hide);
+}
 
-
- 
-
+hide();
+window.addEventListener("scroll", hide);
